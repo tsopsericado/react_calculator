13e3f51fbe7be18ac3895b4e3e23999f345161c7
tsopsericado <ricardotsopse@gmail.com>
2023-07-23 18:15:28 +0100

    numberClick handler function created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+
 
 const btnValues = [
   ["C", "+-", "%", "/"],
@@ -12,19 +14,62 @@ const btnValues = [
 ];
 
 const App=()=> {
+   let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+   });
+
+   const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if (calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num: 
+        calc.num === 0 && value === "0"
+        ? "0"
+        : calc.num % 1 === 0
+        ? Number(calc.num + value)
+        : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+   }
+
+
   return (
-     <Wrapper>
-       <Screen value="0" />
-        <ButtonBox>
-          <Button className=""
-          value="0"
-          onClick={() =>{
-            console.log("Button clicked!");
-          }}
-          />
-        </ButtonBox>
-     </Wrapper>
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res} />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i)=>{
+
+        return (
+        <Button
+          key={i}
+          className={btn === "=" ? "equals" : ""}
+          value={btn}
+          onClick={
+            btn === "C"
+              ? resetClickHandler
+              : btn === "+-"
+              ? invertClickHandler
+              : btn === "%"
+              ? percentClickHandler
+              : btn === "="
+              ? equalsClickHandler
+              : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+              ? signClickHandler
+              : btn === "."
+              ? commaClickHandler
+              : numClickHandler
+          }
+        />
+        );
+        })}
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
